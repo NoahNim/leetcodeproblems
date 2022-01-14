64918606c2f879fe88f4edb20d375bf877f30baf
NoahNim <nmedoff123@gmail.com>
2022-01-14 12:48:25 -0800

    seconf attempt

--- structy/anagram.js
+++ structy/anagram.js
@@ -1,4 +1,14 @@
 const anagrams = (s1, s2) => {
-    if (s1.includes(s2)) return true;
-    else return false
+    let s1count = 0;
+    let s2ount = 0;
+
+    for (let i = 0; i < s1.length, i++) {
+        s1count += 1;
+        for (let j = 0; j < s2.length; j++) {
+            if (s1[i].includes(s2[j])) s2ount += 1;
+        }
+    }
+
+    if (s1count === s2ount) return true;
+    else return false;
 };
