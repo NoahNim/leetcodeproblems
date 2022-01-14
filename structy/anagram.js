7d8a204b7bdd254386dbb365e7ca6493564fb58c
NoahNim <nmedoff123@gmail.com>
2022-01-14 12:51:40 -0800

    third attempt

--- structy/anagram.js
+++ structy/anagram.js
@@ -1,14 +1,12 @@
 const anagrams = (s1, s2) => {
-    let s1count = 0;
-    let s2ount = 0;
+    let s1count = [];
 
-    for (let i = 0; i < s1.length, i++) {
-        s1count += 1;
+    for (let i = 0; i < s1.length; i++) {
         for (let j = 0; j < s2.length; j++) {
-            if (s1[i].includes(s2[j])) s2ount += 1;
+            if (s1[i].includes(s2[j])) s1count.push(s1[i])
         }
     }
 
-    if (s1count === s2ount) return true;
+    if (s1count.length === s2.length) return true;
     else return false;
 };
